c370d35a639fb4bd5a289a108ce715bb88f6ad44
pankaj1touch <Pankaj@kyma.world>
2025-11-09 21:04:23 +0530

    add url

--- apps/backend/src/config/cors.js
+++ apps/backend/src/config/cors.js
@@ -2,7 +2,7 @@ const corsOptions = {
   origin: function (origin, callback) {
     const whitelist = process.env.CLIENT_URL
       ? process.env.CLIENT_URL.split(',')
-      : ['http://localhost:3000','https://bosstradersinvestorclass.com','https://main.d314wrchp9hgrc.amplifyapp.com'];
+      : ['http://localhost:3000','https://bosstradersinvestorclass.com','https://main.d314wrchp9hgrc.amplifyapp.com','https://www.bosstradersinvestorclass.com'];
 
     if (!origin || whitelist.indexOf(origin) !== -1) {
       callback(null, true);
